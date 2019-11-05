ee53f58b3552a2362cf3ef84c06bc282963f0496
amal jose <amal.jose@appsintegra.com>
2019-11-05 09:20:47 +0400

    Trying bem in cra using craco

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './test.less';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container app-container--grey app-container--small">
+      <h1 className="app-container__heading app-container__heading--green">Hello CodeSandbox</h1>
+      <h2 className="app-container__paragraph app-container__paragraph--white">Start editing to see some magic happen!</h2>
     </div>
   );
 }
